70b6537a4acb9cd85b711f50c66ed81e0f7bec3e
Thinh Pham <phamvanthinh2001x@gmail.com>
2021-12-21 09:46:39 +0700

    dynamic pages with templating

--- web-service/src/app.js
+++ web-service/src/app.js
@@ -5,7 +5,15 @@ const port = 3000;
 
 const publicDirectoryPath = path.join(__dirname, "../public");
 
+app.set("view engine", "hbs");
 app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "weather app",
+    name: "Thinh",
+  });
+});
 
 app.get("/about", (req, res) => {
   res.send([{ name: "Thinh", age: 20 }]);
